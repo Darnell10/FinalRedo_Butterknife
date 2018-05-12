e5b8909c5b1402f38d8756ccced6040afd492b30
Darnell O <DarnellOtto@ac.c4q.nyc>
2018-05-11 22:04:26 -0400

    Added login logic, created Breeds activity, Intent to take user to Breeds activity

--- app/src/main/java/com/example/d/finalbutterknife/LoginActivity.java
+++ app/src/main/java/com/example/d/finalbutterknife/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.example.d.finalbutterknife;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +13,10 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String SHARED_PREFS_KEY = "sharedPrefsKey";
+    private SharedPreferences login;
+
 
     @BindView(R.id.user_name)
     EditText editTextUsername;
@@ -26,6 +32,8 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+        login = getApplicationContext().getSharedPreferences(SHARED_PREFS_KEY,MODE_PRIVATE);
+
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,5 +45,26 @@ public class LoginActivity extends AppCompatActivity {
 
     public void verify(){
         Toast.makeText(this,"Clicked",Toast.LENGTH_LONG).show();
+        SharedPreferences.Editor editor = login.edit();
+
+        String checkUserName = editTextUsername.getText().toString();
+        String checkPassword = editTextPassword.getText().toString();
+
+        if(checkUserName.isEmpty()){
+            editTextUsername.setError("Oops , You forgot the Username");
+            editTextUsername.requestFocus();
+        } else if (checkPassword.isEmpty()) {
+            editTextPassword.setError("Oops, You forgot to enter a password");
+            editTextPassword.requestFocus();
+        } else if (checkPassword.contains(checkUserName)){
+            editTextPassword.setError("Password cannot be the same as Username");
+            editTextPassword.requestFocus();
+        } else {
+            Intent intent = new Intent(LoginActivity.this, BreedsActivity.class);
+            intent.putExtra("username",editTextUsername.getText().toString());
+            editor.putString("email", checkUserName);
+            editor.apply();
+            startActivity(intent);
+        }
     }
 }
